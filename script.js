7e65a584e5fa461e7455e24444b820f8e4f6ba43
Jayrajsinhzala1101 <jayrajsinhzala1101@gmail.com>
2025-05-31 12:16:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 let cursor = document.querySelector("#cursor");
 let blr = document.querySelector("#cursor-blur");
 document.addEventListener("mousemove",function(dets){
-    cursor.style.left = dets.x + "px"
+    cursor.style.left = dets.x  + "px"
     cursor.style.top = dets.y + "px"
     blr.style.left = dets.x - 200 + "px"
     blr.style.top = dets.y - 200 + "px"
 })
-
 gsap.to("#nav",{
     backgroundColor:"#000",
-    height:"110px",
+    height:"25%",
     duration:0.5,
     scrollTrigger:{
         trigger:"#nav",
         scroller:"body",
         start:"top -10%",
         end:"top -11%",
+        // markers:true,
         scrub:1
     }
 })
+// gsap.to("#btn1",{
+//     y:"-55%",
+//     scrollTrigger:{
+//         trigger:"#nav",
+//         scroller:"body",
+//         // markers:true,
+//         start:"top -10%",
+//         end:"top -11%",
+//         scrub:1
+//     }
+// })
+// gsap.to("#btn2",{
+//     y:"-55%",
+//     scrollTrigger:{
+//         trigger:"#nav",
+//         scroller:"body",
+//         // markers:true,
+//         start:"top -10%",
+//         end:"top -11%",
+//         scrub:1
+//     }
+// })
+
 gsap.to(".text",{
     backgroundColor:"#000",
     scrollTrigger:{
@@ -30,19 +53,34 @@ gsap.to(".text",{
     }
 })
 
-let i3 = document.querySelector("#i3");
-i3.addEventListener("mousein",function(){
-    innerText="jkfewjkjfiw"
-})
+
 
 gsap.to(".h3text > h3",{
     y:"-120",
     scrollTrigger:{
         trigger:"#photo2",
         scroller:"body",
-        start:"top 20px",
-        end:"top 60px",
-        markers:true,
+        start:"top 85%",
+        end:"top 80%",
+        // markers:true,
         scrub:3
     }
 })
+// gsap.to("#btn1",{
+    
+// })
+
+let navh4 = document.querySelectorAll("#nav > h4")
+navh4.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        cursor.style.scale = 3;
+        cursor.style.border = "1px solid white"
+        cursor.style.backgroundColor = "transparent"
+    })
+    elem.addEventListener("mouseleave",function(){
+        cursor.style.scale = 1;
+        cursor.style.border = "1px solid #91B508"
+        cursor.style.backgroundColor = "#91B508"
+    })
+})
+
